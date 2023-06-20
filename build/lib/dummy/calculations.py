8c691bc287cc8b31cb5d3337947806aa46f6e2df
illumi-410 <sandeepparajuli96@gmail.com>
2023-06-20 15:24:34 +0545

    first 1.0.1

---
+++ build/lib/dummy/calculations.py
@@ -0,0 +1,14 @@
+def greet_user(name):
+    print(f'Hello {name}, welcome to learning python')
+
+def addition(number1, number2):
+    return number1 + number2
+
+def subtraction(number1, number2):
+    return number1 - number2
+
+def multiplication(number1, number2):
+    return number1 + number2
+
+def division(number1, number2):
+    return number1 + number2
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+
+setup(
+    name='dummy',
+    version='1.0.1',
+    description='Dummy python project',
+    author='Sndeep Parajuli',
+    packages=['dummy'],
+
+)
