d09ed510c5eafd69db99f8fdc2e99a588b28614e
illumi-410 <sandeepparajuli96@gmail.com>
2023-06-20 20:23:09 +0545

    greet test

--- dummy/greet.py
+++ dummy/greet.py
@@ -1,3 +1,5 @@
 def greet_user(name):
+    if not isinstance(name, str):
+        return 'Not a string value'
     name = name.strip()
     return f'Hello {name}'
--- tests/test_greet.py
+++ tests/test_greet.py
@@ -3,11 +3,21 @@ from dummy.greet import greet_user
 
 class GreetTestCase(unittest.TestCase):
     def test_greet_user(self):
+
         result = greet_user('John')
         self.assertEqual(result, 'Hello John')
 
         result = greet_user('  John  ')
         self.assertEqual(result, 'Hello John')
+
+        result = greet_user('')
+        self.assertEqual(result, 'Hello ')
+
+        result = greet_user(None)
+        self.assertCountEqual(result, 'Not a string value')
+
+        result = greet_user(['John'])
+        self.assertCountEqual(result, 'Not a string value')
 
     
 
