dfdfd4efc5b427fcab4c71f833de409b2c08a552
illumi-410 <sandeepparajuli96@gmail.com>
2023-06-21 22:57:07 +0545

    paramaterize

--- tests/test_greet.py
+++ tests/test_greet.py
@@ -1,38 +1,16 @@
+import pytest
 from dummy.greet import greet_user
 
-def test_greet_user():
+@pytest.mark.parametrize("greet , expected_output",[
+    ('John', 'Hello John'),
+    ('  John  ', 'Hello John'),
+    ('', 'Hello ' ),
+    (None, 'Not a string value'),
+    ([], 'Not a string value'),
+    ({}, 'Not a string value'),
 
-    expected_output = 'Hello John'
-    assert greet_user('John') == expected_output
+])
+def test_greet_user(greet, expected_output):
+        assert greet_user(greet) == expected_output
 
-    expected_output = 'Hello John'
-    assert greet_user('   John   ') == expected_output
-
-    expected_output = 'Hello '
-    assert greet_user('') == expected_output
-
-    expected_output = 'Not a string value'
-    assert greet_user(None) == expected_output
-
-    expected_output = 'Not a string value'
-    assert greet_user(['John', 'Mary']) == expected_output
-
-
-# class GreetTestCase(unittest.TestCase):
-#     def test_greet_user(self):
-
-#         result = greet_user('John')
-#         self.assertEqual(result, 'Hello John')
-
-#         result = greet_user('  John  ')
-#         self.assertEqual(result, 'Hello John')
-
-#         result = greet_user('')
-#         self.assertEqual(result, 'Hello ')
-
-#         result = greet_user(None)
-#         self.assertCountEqual(result, 'Not a string value')
-
-#         result = greet_user(['John'])
-#         self.assertCountEqual(result, 'Not a string value')
 
